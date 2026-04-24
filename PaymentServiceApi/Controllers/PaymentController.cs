9a5d1bcb7c32a9183aaeae6ae35c64b63903e276
riya.monis <riya.monis@LTPBAN292669673>
2026-04-24 12:53:53 +0530

    Initial commit

---
+++ PaymentServiceApi/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PaymentServiceApi.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
