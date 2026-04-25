601093e33e9a730b6f63bbfbec9ebafe49040329
riya.monis <riya.monis@LTPBAN292669673>
2026-04-25 11:02:09 +0530

    Added project files

---
+++ DTOs/PaymentResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentServiceApi.DTOs
+{
+    public class PaymentResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public int? TransactionId { get; set; }
+    }
+}
---
+++ Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentServiceApi.Models
+{
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionId { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
